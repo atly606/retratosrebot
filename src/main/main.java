750a13dbef4b86e9e2400d45e2bded0de0bc2b38
Leihongkai Ye <atly606@gmail.com>
2018-11-08 14:24:24 +0100

    08/11/2018

---
+++ src/main/main.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) { 
+	Scanner in = new Scanner(System.in);
+	int opcionPelo;
+	do {
+		System.out.println("1 - WWWWWWWWW");
+		System.out.println("2 - \\\\\\//////");
+		System.out.println("3 - |\"\"\"\"\"\"\"|");
+		System.out.println("4 - ||||||||");
+		System.out.println("Introduce el numero de la opción elegida (1-4)");
+		opcionPelo = in.nextInt();
+		if (opcionPelo < 1 || opcionPelo > 4)
+			System.out.println("La opción elegida no es correcta.");
+		
+	} while (opcionPelo <1 || opcionPelo > 4);
+}
+  
+  
+    
+    int opcionOjos;
+    do {
+    	System.out.println("1 - | O O |");
+    	System.out.println("2 - |-(· ·)-| ");
+    	System.out.println("3 - |-(o o)-|");
+    	System.out.println("4 - | \\ / |");
+    	System.out.println("Introduce el numero de la opción elegida (1-4)");
+    	opcionOjos = in.nextInt();
+    	if (opcionOjos < 1 || opcionOjos > 4 );
+    	  System.out.println("La opción elegida no es correcta.");
+    	  
+    } while (opcionOjos < 1 || opcionOjos > 4);
+    }
+    
+
+    int opcionOrejayNariz;
+    do {
+    	System.out.println("1 - @  J  @");
+    	System.out.println("2 - { \" }");
+    	System.out.println("3 - [ j ]");
+    	System.out.println("4 - < - >");
+    	System.out.println("Introduce el numero de la opción elegida (1-4)");
+    	opcionOrejayNariz = in.nextInt();
+    	if (opcionOrejayNariz < 1 || opcionOrejayNariz > 4 );
+    	  System.out.println("La opción elegida no es correcta.");
+    } while (opcionOrejayNariz < 1 || opcionOrejayNariz > 4);
+    }
+    
+    int opcionBoca;
+    do {
+    	System.out.println("1 - | === |");
+    	System.out.println("2 - | - |");
+    	System.out.println("3 - | ___ |");
+    	System.out.println("4 - | --- |");
+    	System.out.println("Introduce el numero de la opción elegida (1-4) ");
+    	opcionBoca = in.nextInt();
+    	if (opcionBoca < 1 || opcionBoca > 4 );
+    	System.out.println("La opción elegida no es correcta.");
+    } while (opcionBoca < 1 || opcionBoca > 4 );
+    }
+    
