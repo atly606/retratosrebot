e2da624e2a3c629cba3f7fb89f4569283232689a
Leihongkai Ye <atly606@gmail.com>
2018-11-27 08:57:03 +0100

    27/11/2018

---
+++ src/Prueba/Prueba1.java
@@ -0,0 +1,18 @@
+package Prueba;
+
+public class Prueba1 {
+   
+	public static void main(String[] args) {
+		String [] dias;
+		dias = new String[7];
+		dias[0] = "lunes";
+		dias[1] = "martes";
+		dias[2] = "miércoles";
+		dias[3] = "jueves";
+		dias[4] = "viernes";
+		dias[5] = "sábado";
+		dias[6] = "domingo";
+		for(int i=0; i<dias.length; i++)
+			System.out.println(dias[1]);
+	}
+}
